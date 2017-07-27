2dd750b3bfc93eeacdc4353b1685842401ac4a75
zhangkaina <zhangkaina@unitedstack.com>
2017-07-27 18:18:10 +0800

    halo-website

--- _site/js/control.js
+++ _site/js/control.js
@@ -16,3 +16,26 @@ $('.header-center .multi > a').click(function() {
   $(this).next().slideToggle(300);
   $(this).toggleClass('second-open');
 });
+var G = {
+  header: $('.nav-header'),
+  toTop: $('#totop'),
+  bodyCST: document.body.scrollTop || document.documentElement.scrollTop,
+};
+  G.bodyPST = G.bodyCST;
+$(window).on({
+  scroll: function() {
+    G.bodyPST = G.bodyCST;
+    G.bodyCST = document.body.scrollTop || document.documentElement.scrollTop;
+    G.bodyDir = G.bodyPST > G.bodyCST ? -1 : 1;
+    if(G.bodyCST > 800) {
+      G.toTop.css({opacity: 1});
+    } else {
+      G.toTop.css({opacity: 0});
+    }
+  }
+});
+  $(G.toTop).click(function() {
+  $('html, body').animate({
+    scrollTop: 0
+  }, 500);
+});
